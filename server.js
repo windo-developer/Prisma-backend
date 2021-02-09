033003b7bead51943e5a5eddebd17001ccf8ae9b
windo-developer <yui050505@gmail.com>
2021-02-09 16:51:04 +0900

    connent DB

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+
+import schema from "./schema";
+
+const server = new ApolloServer({
+  schema,
+});
+
+const PORT = process.env.PORT;
+
+server
+  .listen(PORT)
+  .then(() => console.log(`Server is running http://localhost:${PORT}/`));
