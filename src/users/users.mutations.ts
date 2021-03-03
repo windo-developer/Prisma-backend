938caef39cd6573c8602233509fa62fa08df5e98
windo-developer <yui050505@gmail.com>
2021-03-03 01:18:44 -0800

    login

--- src/users/users.mutations.ts
+++ src/users/users.mutations.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import * as bcrypt from "bcrypt";
 
 import client from "../client";
 
@@ -8,29 +8,49 @@ export default {
       _,
       { firstName, lastName, username, email, password }
     ) => {
-      // check username or email already on DB
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              username,
-            },
-            {
-              email,
-            },
-          ],
-        },
-      });
-      const hashedPassword = await bcrypt.hash(password, 10);
-      return client.user.create({
-        data: {
-          username: username,
-          email: email,
-          firstName: firstName,
-          lastName: lastName,
-          password: hashedPassword,
-        },
-      });
+      try {
+        // check username or email already on DB
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username or password is already taken.");
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            username: username,
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            password: hashedPassword,
+          },
+        });
+      } catch (error) {
+        return error;
+      }
+    },
+    login: async (_, { username, password }) => {
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return { ok: false, error: "user not found" };
+      }
+      const passwordCheck = await bcrypt.compare(password, user.password);
+      if (!passwordCheck) {
+        return {
+          ok: false,
+          error: "Incorrect password",
+        };
+      }
     },
   },
 };
