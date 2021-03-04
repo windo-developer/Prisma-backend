89037db1e02879f0c39e586009d92130709c5405
windo-developer <yui050505@gmail.com>
2021-03-04 00:11:35 -0800

    eject apollo server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
+import * as express from "express";
+import * as logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
 
 import { typeDefs, resolvers } from "./schema";
 import { getUser, protectResolver } from "./users/users.utils";
+
+const PORT = process.env.PORT;
 
 const server = new ApolloServer({
   typeDefs,
@@ -10,9 +14,16 @@ const server = new ApolloServer({
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.authorization),
-      protectResolver,
     };
   },
 });
 
-server.listen().then(({ url }) => console.log(`Server is running ${url}`));
+const app = express();
+app.use(logger("tiny"));
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () => {
+  console.log(
+    `Apollo Express Server running on http://localhost:${PORT}/graphql`
+  );
+});
